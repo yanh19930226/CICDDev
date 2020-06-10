9b51b77886f5d025cb0db7063373a8a59c54bc31
yanh <891367701@qq.com>
2020-06-10 14:55:23 +0800

    test

--- src/CICDApi/Startup.cs
+++ src/CICDApi/Startup.cs
@@ -18,7 +18,9 @@ namespace CICDApi
         {
             Configuration = configuration;
         }
-
+        /// <summary>
+        /// test
+        /// </summary>
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
